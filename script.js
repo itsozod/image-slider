e2522b454748793030b8e2a808bef7a6a267b851
Ozod <bukhorizodao@gmail.com>
2023-07-11 19:18:34 +0500

    fixed prev and next buttons

--- script.js
+++ script.js
@@ -2,43 +2,62 @@ const slider = document.querySelector(".slider");
 const leftArrow = document.querySelector(".left");
 const rightArrow = document.querySelector(".right");
 const controlBullets = document.querySelectorAll(".controls li");
+const indicatorParents = document.querySelector(".controls ul");
+
+// leftArrow.style.opacity = "0.7";
+// rightArrow.style.opacity = "1";
 
 // eslint-disable-next-line prefer-const
 let sectionIndex = 0;
 
+function setIndex() {
+  const selectedBullets = document.querySelector(".controls .selected");
+  selectedBullets.classList.remove("selected");
+  // eslint-disable-next-line prefer-template
+  slider.style.transform = "translate(" + sectionIndex * -25 + "%)";
+}
 controlBullets.forEach((indicator, index) => {
     indicator.addEventListener("click", () => {
         sectionIndex = index;
-        const selectedBullets = document.querySelector(".controls .selected");
-        selectedBullets.classList.remove("selected");
+        // const selectedBullets = document.querySelector(".controls .selected");
+        // selectedBullets.classList.remove("selected");
+        setIndex();
         indicator.classList.add("selected");
         // eslint-disable-next-line prefer-template
-        slider.style.transform = "translate(" + index * -25 + "%)";
+        // slider.style.transform = "translate(" + index * -25 + "%)";
 
     })
 });
 
 leftArrow.addEventListener("click", () => {
   // eslint-disable-next-line prefer-template, operator-assignment
-//   sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 0;
+  // sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 0;
 if (sectionIndex > 0) {
     sectionIndex -= 1;
   } else {
     sectionIndex = 0;
   }
-  
+  // const selectedBullets = document.querySelector(".controls .selected");
+  // selectedBullets.classList.remove("selected");
+  setIndex();
+  indicatorParents.children[sectionIndex].classList.add("selected");
   // eslint-disable-next-line prefer-template
-  slider.style.transform = "translate(" + sectionIndex * -25 + "%)";
+  // slider.style.transform = "translate(" + sectionIndex * -25 + "%)";
 });
 
 rightArrow.addEventListener("click", () => {
   // eslint-disable-next-line prefer-template, operator-assignment
-//   sectionIndex = (sectionIndex < 3) ? sectionIndex + 1 : 3;
+  // sectionIndex = (sectionIndex < 3) ? sectionIndex + 1 : 3;
 if (sectionIndex < 3) {
     sectionIndex += 1;
   } else {
     sectionIndex = 3;
   }
+  // const selectedBullets = document.querySelector(".controls .selected");
+  // selectedBullets.classList.remove("selected");
+  setIndex();
+  indicatorParents.children[sectionIndex].classList.add("selected");
+
   // eslint-disable-next-line prefer-template
-  slider.style.transform = "translate(" + sectionIndex * -25 + "%)";
+  // slider.style.transform = "translate(" +  sectionIndex * -25 + "%)";
 });
